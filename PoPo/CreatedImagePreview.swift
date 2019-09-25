799b602b819a98a7f63c7e155aa18a37c1e8eb4d
Yoshiyoshida <yoshiyoshida0213@gmail.com>
2019-09-25 17:01:23 +0900

    CreatedImagePreview

---
+++ PoPo/CreatedImagePreview.swift
@@ -0,0 +1,17 @@
+//
+//  CreatedImagePreview.swift
+//  PoPo
+//
+//  Created by 吉田侑生 on 2019/09/25.
+//  Copyright © 2019 吉田侑生. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreatedImagePreview: UIViewController{
+    
+    @IBAction func backTapped() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
